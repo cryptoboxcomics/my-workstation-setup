d8cd26895d6021400d592dc5398297ada50c4381
cryptoboxcomics <cryptoboxcomics@gmail.com>
2021-04-07 23:12:22 -0400

    more stuff

--- clean-gnome-debian.sh
+++ clean-gnome-debian.sh
@@ -20,3 +20,5 @@ git clone --branch debian https://github.com/ChrisTitusTech/titus-awesome ~/.con
 mkdir -p ~/.config/rofi
 cp $HOME/.config/awesome/theme/config.rasi ~/.config/rofi/config.rasi
 sed -i '/@import/c\@import "'$HOME'/.config/awesome/theme/sidebar.rasi"' ~/.config/rofi/config.rasi
+
+cp .xinitrc $HOME/.xinitrc
